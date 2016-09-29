2a9b5e712483ef29c1737c7220962e8356f11afa
Darko Rajin <rajindarko@gmail.com>
2016-09-29 15:09:30 +0200

    IDEA Test runner does what we want and we have one button testing

---
+++ test/source/com/darivoje/setup/test/HookupTest.java
@@ -0,0 +1,16 @@
+package com.darivoje.setup.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by darkorajin on 29.09.16.
+ */
+public class HookupTest {
+    @Test
+    public void isThisThingOn() throws Exception {
+
+
+    }
+}
