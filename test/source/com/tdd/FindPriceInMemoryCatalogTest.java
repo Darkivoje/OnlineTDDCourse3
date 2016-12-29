210d85fe71084f5b35ea1bbc8862bb46cfb8d5e3
Darko Rajin <rajindarko@gmail.com>
2016-12-29 11:15:24 +0100

    We can find a price if a price is there to be foundl

---
+++ test/source/com/tdd/FindPriceInMemoryCatalogTest.java
@@ -0,0 +1,35 @@
+package com.tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Created by darkorajin on 29/12/2016.
+ */
+public class FindPriceInMemoryCatalogTest {
+
+    @Test
+    public void productFound() throws Exception {
+        Price foundPrice = Price.cents(1250);
+
+        InMemoryCatalog catalog = new InMemoryCatalog(Collections.singletonMap("12345", foundPrice));
+        Assert.assertEquals(foundPrice, catalog.findPrice("12345"));
+
+    }
+
+    public static class InMemoryCatalog {
+        private Map<String, Price> pricesByBarcode;
+
+        public InMemoryCatalog(Map<String, Price> pricesByBarcode) {
+
+            this.pricesByBarcode = pricesByBarcode;
+        }
+
+        public Price findPrice(String barcode) {
+            return pricesByBarcode.get(barcode);
+        }
+    }
+}
